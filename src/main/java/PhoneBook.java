5aa13cd4d769ac8759f61029acdb87f23115d1b1
Andrew <aka.slurm@gmail.com>
2022-11-20 20:38:58 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,7 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
-    public boolean add(){
-        return false;
+    private final Map<String, Long> phonebook = new HashMap<>();
+
+    public int add(String name, Long phone) {
+        phonebook.put(name, phone);
+        return phonebook.size();
     }
 
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,21 +1,26 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 public class PhoneBookTest {
 
     @Test
     public void addOnePhone() {
         PhoneBook phoneBook = new PhoneBook();
-        assertEquals(1, phoneBook.add(),"Должен вернуться один номер");
+        String name = "Jon";
+        long phone = 89031114422L;
+        assertEquals(1, phoneBook.add(name, phone), "Должен вернуться один номер");
     }
 
     @Test
-    public void addTwoEqualsPhomnes(){
+    public void addTwoEqualsPhomnes() {
         PhoneBook phoneBook = new PhoneBook();
-        assertEquals(1,phoneBook.add(),"Должен вернуться один номер, дубли не храним");
+        String name = "Jon";
+        long phone1 = 89031112222L;
+        long phone2 = 89031114422L;
+
+        assertEquals(1, phoneBook.add(name, phone1), "Должен вернуться один номер, дубли не храним");
+        assertEquals(1, phoneBook.add(name, phone2), "Должен вернуться один номер, дубли не храним");
     }
 
 }
